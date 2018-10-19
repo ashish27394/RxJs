d17c20ea93a14a9d07782c0c1b5bfdf7ee478694
Mohini Bala <mbala@colibrium.com>
2018-10-19 17:01:43 +0530

    Observable, observer and Subscriber

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-alert("Rxjs");
+import {Observable} from 'rxjs';
+import {allBooks} from './data';
+
+// One way
+function subscribe(subscriber){
+    for(let book of allBooks){
+        subscriber.next(book);
+    }
+}
+
+let allBookObservable$ = new Observable(subscribe);
+
+allBookObservable$.subscribe(book => console.log(book.title));
+
